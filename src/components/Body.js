a884fce95d3f958a5b1597e7963cedcde17299ae
Anirudha Kharabe <anirudha.kharabe@realpage.com>
2025-11-13 09:03:29 +0530

    useEffect

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 
 const Body = () => {
-  const  [listOfResteaurants, setListOfRestaurants] = useState(resList)
+  const  [listOfResteaurants, setListOfRestaurants] = useState([]);
+
+  useEffect(()=>{
+    fetchData();
+  }, [])
+
+  const fetchData = async() => {
+    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=18.5288974&lng=73.8665321&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    const json = await data.json();
+    console.log(json);
+    setListOfRestaurants(json.data.cards[1].card.card.gridElements.infoWithStyle.restaurants);
+  }
 
 
-  return (
+  return listOfResteaurants.length === 0 ? <h1>Loading/Fetching Restaurants Data...</h1> :(
     <div className="body">
       <div className="filter">
         <button
