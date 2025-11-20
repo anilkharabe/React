8e8d78c2843c5a65306c1f52fcc34890ad9f881e
Anirudha Kharabe <anirudha.kharabe@realpage.com>
2025-11-20 08:39:47 +0530

    Showing res details

--- src/components/RestaurantDetails.js
+++ src/components/RestaurantDetails.js
@@ -1,53 +1,53 @@
-import { useEffect } from "react";
-import { useParams } from 'react-router-dom'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import resDetails from "../utils/resDetails";
 
-const RestaruntDetails = ()=>{
+const RestaruntDetails = () => {
+  const { resId, resName } = useParams();
+  const [resInfo, setResInfo] = useState(resDetails);
 
-    const {resId, resName} = useParams();
+  const {name, avgRating, totalRatings, cuisines, areaName} = resInfo.data.cards[2].card.card.info;
+  const {itemCards} = resInfo.data.cards[4].groupedCard.cardGroupMap.REGULAR.cards[2].card.card;
 
-    useEffect(()=>{
-        // fetchData();
-    },[])
+  useEffect(() => {
+    // fetchData(); // not working as expected , try it later
+  }, []);
+  console.log('rendering', resInfo)
 
-const fetchData = async () => {
-  try {
-    const res = await fetch(
-      `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5288974&lng=73.8665321&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
-    );
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5288974&lng=73.8665321&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER`
+      );
 
-    if (!res.ok) {
-      throw new Error(`HTTP Error! Status: ${res.status}`);
+      const data = await res.json();
+
+      setResInfo(data);
+    } catch (err) {
+      console.error("Error fetching data:", err);
     }
+  };
 
-    const text = await res.text();   // read response as text
-    console.log("Raw response:", text);
 
-    const jsonData = JSON.parse(text);
-    console.log(jsonData);
-
-  } catch (err) {
-    console.error("Error fetching data:", err);
-  }
+  return (
+    <div>
+      <h1> {name}</h1>
+      <h4> {avgRating}{"   ("+ totalRatings+"+ratings"+")"}</h4>
+      <h4> {cuisines.join(', ')}</h4>
+      <h4> {areaName}</h4>
+      
+      <div>
+        <h1>Menu</h1>
+        <ul>
+          {itemCards.map((menu)=>{
+            return <li key={menu.card.info.id}>{menu.card.info.name} for ₹{menu.card.info.price/100}</li>
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
-    return (
-        <div>
-            <h1>Restaurant Details</h1>
-            <h1> {resName}</h1>
-            <div>
-                <h1>Menu</h1>
-                <ul>
-                    <li>Poha</li>
-                    <li>Dosa</li>
-                    <li>Misal Pav</li>
-                </ul>
-            </div>
-        </div>
-    )   
-}
-
 export default RestaruntDetails;
-
-
 
 // https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=18.5288974&lng=73.8665321&restaurantId=${resId}&catalog_qa=undefined&submitAction=ENTER
